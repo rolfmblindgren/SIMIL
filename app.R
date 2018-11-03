b87d7668222b1ae98ec6979909d31661f2d28d0f
Rolf Marvin Bøe Lindgren <rolf@grendel.no>
2018-11-03 16:06:22 +0100

    Forklarende tekst.

--- app.R
+++ app.R
@@ -12,7 +12,7 @@ library(shiny)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
-    titlePanel("Yrkestilpasningskalkulator:"),
+    titlePanel("Parmatch!:"),
 
     sidebarLayout(
         sidebarPanel(
@@ -70,7 +70,7 @@ ui <- fluidPage(
                                    max = 80,
                                    value = 50)))),
         mainPanel(
-            textOutput("result")
+            htmlOutput("result")
         )
     )
 )
@@ -96,8 +96,13 @@ server <- function(input, output) {
         IVm <- input$IVm
         Vm <- input$Vf
 
-        round(100-proxy::dist(rbind(c(If,IIf,IIIf,IVf,Vf),c(Im,IIm,IIIm,IVm,Vm)),
-                    method="Euclidean")/120*100,0)
+        result <- round(
+            100-proxy::dist(rbind(c(If,IIf,IIIf,IVf,Vf),c(Im,IIm,IIIm,IVm,Vm)),
+                            method="Euclidean")/120*100,0)
+
+        paste0(p("Match mellom skårer er ", result , " prosent."),
+               p("Par blir typisk sammen på grunn av likheter i interesser, menholder sammen på grunn av likheter i personlighet. Basert på Grendels eksklusive forsking viser det seg at hvis match er fra ca 75%, har forholdet svært gode sjanser. Er skåren lavere, holder dere sammen på egen risiko."))
+        
     })
 }
 
